b4de3b9a50961a430b99bd8c9a9c34d917b32465
Lukaz Rocha <lukazrocha@codefire.com.br>
2021-09-06 13:24:14 -0300

    organiza elementos no banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,8 +4,15 @@ import "./Banner.scss"
 class Banner extends Component{
     render(){
         return(
-            <div className="banner vh-100">
-                <p>Codefire</p>
+            <div className="content-center">
+                <div className="banner vh-100 overflow-hidden">
+                    <div className="container-banner">
+                        <span>Codefire</span>
+                        <div className="badge">
+                            <span>dev</span>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }    
